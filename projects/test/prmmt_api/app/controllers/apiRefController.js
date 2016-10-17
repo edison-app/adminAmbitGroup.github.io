5208726c34c7c3afd064bd81e2d14e37b8480c16
adminAmbitGroup <dthor@theambitgroup.com>
2016-10-17 16:21:54 -0400

    Add files via upload

--- projects/test/prmmt_api/app/controllers/apiRefController.js
+++ projects/test/prmmt_api/app/controllers/apiRefController.js
@@ -1,6 +1,6 @@
 angular.module('app.features.controllers')
 .controller("ApiController", ['$scope','$http', function($scope,$http) {   
-        $http.get("../json_files/api_list.json")
+        $http.get("js/json_files/api_list.json")
         .then(function(response){
             $scope.apiReferences = response.data.apiReferences;
         });
