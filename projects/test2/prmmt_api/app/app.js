8f095533b74247541a080d21be301b72dc832ff1
adminAmbitGroup <dthor@theambitgroup.com>
2016-10-26 10:41:44 -0400

    Add files via upload

--- projects/test2/prmmt_api/app/app.js
+++ projects/test2/prmmt_api/app/app.js
@@ -2,15 +2,4 @@
 
 
 //angular.module('app', ['app.features.controllers']);
-angular.module('app', ['app.features.controllers','ngRoute'])
-.config(['$routeProvider',
-  function(
-    $routeProvider
-) {
-      $routeProvider.
-        when('/{{apiReference.methods}}',{
-            templateUrl: '/views/{{apiReference.links}}.html',
-            controller:'RefRouteController'
-        })
-
- }])
+angular.module('app', ['app.features.controllers']);
