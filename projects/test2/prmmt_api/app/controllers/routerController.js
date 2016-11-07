cef97bbeef408ad4e5e39aac1e67db4e32d18ece
adminAmbitGroup <dthor@theambitgroup.com>
2016-11-07 16:37:14 -0500

    Add files via upload

--- projects/test2/prmmt_api/app/controllers/routerController.js
+++ projects/test2/prmmt_api/app/controllers/routerController.js
@@ -1,7 +1,32 @@
-angular.module('app.features.controllers')
+/*angular.module('app.features.controllers')
 .controller("RefRouteController", ['$scope','$location', function($scope,$location) {   
    $scope.showApiDetails = function(apiReference) {
         $location.path('/view/' + apiReference.link);
     };
-}]);
+}]);*/
 
+angular.module('app.features.controllers')
+.config(function($routeProvider){
+  $routeProvider
+    .when("/views' + {{apiReferences.links}}", {
+        templateUrl : "/views/{{apiReferences.links}}.html",
+        controller : "RefRouteController"
+    });
+});
+
+angular.module('app.features.controllers')
+.controller("RefRouteController", ['$scope','$location', '$locationProvider','$window',function($scope,$location,$locationProvider) {
+   $http.get("js/json_files/api_list.json")
+        .then(function(response){
+            $scope.apiReferences = response.data.apiReferences;
+        })
+        .then(function(response){
+         $locationProvider.html5Mode(true);
+             //call the parameter json - reference:""''
+            var getRefParam = $location.search().reference;
+            if(getRefParam === {{apiReferences.links}}){
+             //do your logic
+             $location.path('#views/' + {{apiReferences.links}});
+            };
+        });
+}]);
