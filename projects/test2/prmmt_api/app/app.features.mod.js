b8ece2c8767cdc82af827de07763c08635cb6b3b
adminAmbitGroup <dthor@theambitgroup.com>
2016-11-22 15:34:21 -0500

    Update app.features.mod.js

--- projects/test2/prmmt_api/app/app.features.mod.js
+++ projects/test2/prmmt_api/app/app.features.mod.js
@@ -1,1 +1,7 @@
-angular.module('app.features.controllers', []);
+angular.module('app.features.controllers', ['ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+  .when("/getChartFilterData", {
+      templateUrl : "views/getChartFilterData.html"
+  });
+});
