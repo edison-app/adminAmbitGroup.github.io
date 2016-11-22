55143299a9ee0fa71a774b19aee2a82681f6b44d
adminAmbitGroup <dthor@theambitgroup.com>
2016-11-22 15:34:51 -0500

    Update app.js

--- projects/test2/prmmt_api/app/app.js
+++ projects/test2/prmmt_api/app/app.js
@@ -1,8 +1,1 @@
-angular.module('app', [
-  'app.features.controllers','ngRoute'])
-.config(function($routeProvider) {
-    $routeProvider
-    .when("/getChartFilterData", {
-        templateUrl : "views/getChartFilterData.html"
-    })
-});
+angular.module('app', ['app.features.controllers']);
