c4e8830adeabf57639d42200ffe7028e804a3887
adminAmbitGroup <dthor@theambitgroup.com>
2016-11-22 13:13:22 -0500

    Add files via upload

--- projects/test/prmmt_api/new_prmmt/prmmt_api/app/controllers/apiRefController.js
+++ projects/test/prmmt_api/new_prmmt/prmmt_api/app/controllers/apiRefController.js
@@ -1,7 +1,21 @@
 
 angular.module('app.features.controllers')
+.factory('ApiLoadService', ['$scope','$http', function($scope,$http) {
+   var apiInfoFactory = {};
+   
+   apiInfoFactory.loadApiInfo = function() {
+       $http.get("js/json_files/api_list.json")
+        .then(function(response){
+            $scope.apiReferences = response.data.apiReferences;
+        });
+   }
+   return apiInfoFactory;
+}
+]); 
+
+angular.module('app.features.controllers')
 .controller("ApiController", ['$scope','$http','ApiLoadService', function($scope,$http,ApiLoadService) {   
-        $scope.loadApiInfo = function() {
+        $scope.loadApiInfo = function() {       
             ApiLoadService.loadApiInfo();
         }
     }
