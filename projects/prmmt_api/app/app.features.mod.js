745abc4cf838fbea89fe1b179dbdf1c0a78b2747
adminAmbitGroup <dthor@theambitgroup.com>
2016-11-23 09:19:42 -0500

    Add files via upload

--- projects/prmmt_api/app/app.features.mod.js
+++ projects/prmmt_api/app/app.features.mod.js
@@ -1,1 +1,9 @@
-angular.module('app.features.controllers', []);
+angular.module('app.features.controllers', ['ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+  .when("/getGbiOffNameFilterChartData", {
+      templateUrl : "views/getGbiOffNameFilterChartData.html"
+  });
+});
+
+/*angular.module('app.features.controllers', []);*/
