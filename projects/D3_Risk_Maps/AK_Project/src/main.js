6e08ec6e89b6cfb1e0c851d025cda0b1fb83daf4
adminAmbitGroup <dthor@theambitgroup.com>
2017-01-27 13:38:10 -0500

    Add files via upload

--- projects/D3_Risk_Maps/AK_Project/src/main.js
+++ projects/D3_Risk_Maps/AK_Project/src/main.js
@@ -1,73 +1,57 @@
 /******************************/
 //**** Author David Thor ******/
 /******************************/
-/*This file holds scripts outside of tooltip.js and bubble_chart.js. This includes
+/*This file holds scripts outside of risk_map.js. This includes
 - Text Fade Out/In
 */
 
 var $j = jQuery.noConflict();
-$j(document).ready(function(){
-  //Function to display text for each button/link
-    textHandler();
+$j(document).ready(function () {
+        //Function to display text for each button/link
+        legendHandler();
 });
 
 
-function textHandler(){
-    $j("#all").click(function(){
-        hideFormDiscText();
-        hidePrinOffText();
-        showAllText();
-    });
-
-    $j("#formdisc").click(function(){
-        hideAllText();
-        hidePrinOffText();
-        showFormDiscText();
-    });
-
-    $j("#prinoff").click(function(){
-        hideAllText();
-        hideFormDiscText();
-        showPrinOffText();
-    });
-}
-
-function showAllText(){
-        $j(".alltext").fadeIn("slow");
-        $j("#legendcontainer").css({
-                "top":"596px",
-                "margin-left":"20px"
+function legendHandler() {
+        $j("#datamap").click(function () {
+                showLegend1();
+                hideLegend2();
+                hideLegend3();
         });
-        $j("#legendcircletext").css({"display":"inline"});
-}
 
-function hideAllText(){
-        $j(".alltext").fadeOut("3000");
-}
-
-function showFormDiscText(){
-        $j(".formdisctext").fadeIn("slow");
-
-        $j("#legendcontainer").css({
-                "top":"580px",
-                "margin-left":"330px"
+        $j("#datamap2").click(function () {
+                showLegend2();
+                hideLegend1();
+                hideLegend3();
         });
-        $j("#legendcircletext").css({"display":"inline"});
-}
 
-function hideFormDiscText(){
-        $j(".formdisctext").fadeOut("3000");
-}
-
-function showPrinOffText(){
-        $j(".prinofftext").fadeIn("slow");
-        $j("#legendcontainer").css({
-                "top":"578px",
-                "margin-left":"670px"
+        $j("#datamap3").click(function () {
+                showLegend3();
+                hideLegend1();
+                hideLegend2();
         });
-      //  $j("#legendcircletext").css({"display":"none"});
 }
 
-function hidePrinOffText(){
-        $j(".prinofftext").fadeOut("3000");
+function showLegend1() {
+        $j("#legendmap1").css({ "display": "inline" });
+}
+
+function showLegend2() {
+        $j("#legendmap2").css({ "display": "inline" });
+}
+
+function showLegend3() {
+        $j("#legendmap3").css({ "display": "inline" });
+}
+
+function hideLegend1() {
+        $j("#legendmap1").fadeOut("3000").css({ "display": "none" });
+}
+
+function hideLegend2() {
+        $j("#legendmap2").fadeOut("3000").css({ "display": "none" });
+}
+
+function hideLegend3() {
+        $j("#legendmap3").fadeOut("3000").css({ "display": "none" });
 }
