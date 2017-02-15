7f23483c076b14f810aff1fdb51a941385ccfabb
adminAmbitGroup <dthor@theambitgroup.com>
2017-02-15 17:18:18 -0500

    Add files via upload

--- projects/splash/app/controllers/d3controllers.js
+++ projects/splash/app/controllers/d3controllers.js
@@ -1,11 +1,10 @@
 features.controller('bubbleCtrl', ['$scope', function($scope) {
+  //Data for circles
    $scope.bubbleData = [
-   { "x_axis": 300, "y_axis": 100, "radius": 60, "color" : "red" },
-   { "x_axis": 120, "y_axis": 180, "radius": 60, "color" : "orange"},
-   { "x_axis": 480, "y_axis": 180, "radius": 60, "color" : "yellow"},
-   { "x_axis": 300, "y_axis": 500, "radius": 60, "color" : "green" },
-   { "x_axis": 120, "y_axis": 380, "radius": 60, "color" : "blue"},
-   { "x_axis": 480, "y_axis": 380, "radius": 60, "color" : "violet"}]
+   { "x_axis": 300, "y_axis": 100, "radius": 60, "color" : "red","image": "globe" },
+   { "x_axis": 120, "y_axis": 180, "radius": 60, "color" : "#fff","image": "bubble"},
+   { "x_axis": 480, "y_axis": 180, "radius": 60, "color" : "#fff","image": "globe"},
+   { "x_axis": 300, "y_axis": 500, "radius": 60, "color" : "#fff","image": "bubble"},
+   { "x_axis": 120, "y_axis": 380, "radius": 60, "color" : "#fff","image": "bubble"},
+   { "x_axis": 480, "y_axis": 380, "radius": 60, "color" : "#fff","image":"globe"}]
 }]);
-
-
