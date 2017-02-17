23c44b779cd6f29992c5c442dd7fe5dd56778aa9
adminAmbitGroup <dthor@theambitgroup.com>
2017-02-17 11:21:51 -0500

    Add files via upload

--- projects/splash/app/controllers/d3controllers.js
+++ projects/splash/app/controllers/d3controllers.js
@@ -1,7 +1,7 @@
 features.controller('bubbleCtrl', ['$scope', function($scope) {
   //Data for circles
    $scope.bubbleData = [
-   { "x_axis": 300, "y_axis": 100, "radius": 60, "color" : "red","image": "bubble","url":"https://www.google.com}"},
+   { "x_axis": 300, "y_axis": 100, "radius": 60, "color" : "red","image": "bubble","url":"https://www.google.com"},
    { "x_axis": 120, "y_axis": 180, "radius": 60, "color" : "#fff","image": "pie","url":"https://www.google.com"},
    { "x_axis": 480, "y_axis": 180, "radius": 60, "color" : "blue","image": "map","url":"https://www.google.com" },
    { "x_axis": 300, "y_axis": 500, "radius": 60, "color" : "#fff","image": "tree","url":"https://www.google.com" },
