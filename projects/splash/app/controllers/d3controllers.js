4337a7dc92a661f2553b12461ddd11896a121921
adminAmbitGroup <dthor@theambitgroup.com>
2017-02-17 11:13:28 -0500

    Add files via upload

--- projects/splash/app/controllers/d3controllers.js
+++ projects/splash/app/controllers/d3controllers.js
@@ -1,10 +1,10 @@
 features.controller('bubbleCtrl', ['$scope', function($scope) {
   //Data for circles
    $scope.bubbleData = [
-   { "x_axis": 300, "y_axis": 100, "radius": 60, "color" : "red","image": "globe" },
-   { "x_axis": 120, "y_axis": 180, "radius": 60, "color" : "#fff","image": "pie"},
-   { "x_axis": 480, "y_axis": 180, "radius": 60, "color" : "blue","image": "map"},
-   { "x_axis": 300, "y_axis": 500, "radius": 60, "color" : "#fff","image": "tree"},
-   { "x_axis": 120, "y_axis": 380, "radius": 60, "color" : "#fff","image": "globe"},
-   { "x_axis": 480, "y_axis": 380, "radius": 60, "color" : "#fff","image":"pie"}]
+   { "x_axis": 300, "y_axis": 100, "radius": 60, "color" : "red","image": "bubble","url":"https://www.google.com }"},
+   { "x_axis": 120, "y_axis": 180, "radius": 60, "color" : "#fff","image": "pie","url":"https://www.google.com"},
+   { "x_axis": 480, "y_axis": 180, "radius": 60, "color" : "blue","image": "map","url":"https://www.google.com" },
+   { "x_axis": 300, "y_axis": 500, "radius": 60, "color" : "#fff","image": "tree","url":"https://www.google.com" },
+   { "x_axis": 120, "y_axis": 380, "radius": 60, "color" : "#fff","image": "globe","url":"https://www.google.com" },
+   { "x_axis": 480, "y_axis": 380, "radius": 60, "color" : "#fff","image":"scatter","url":"https://www.google.com" }]
 }]);
